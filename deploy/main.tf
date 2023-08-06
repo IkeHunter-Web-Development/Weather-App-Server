e8f5fb16ab1c8ca44c0f91776c6175aa8c72e7d0
Isaac Hunter <ikehunter5@gmail.com>
2023-08-06 00:44:03 -0400

    create initial tf setup

---
+++ deploy/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "weather-wise-bucket"
+    key            = "weather-app.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "weather-wise-tf-state-lock"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  version = "~>4.67.0"
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
